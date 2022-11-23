c32110bfa1a3007e4cb34c075a3dccabd3fdc726
Mark Burggraf <markb@mantisbible.com>
2022-11-23 11:10:49 -0800

    updated rules in eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,7 @@ module.exports = {
 	ignorePatterns: ['*.cjs'],
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	settings: {
-		'svelte3/typescript': () => require('typescript'),
+		'svelte3/typescript': () => require('typescript')
 	}, 
 	parserOptions: {
 		sourceType: 'module',
@@ -18,7 +18,13 @@ module.exports = {
 		node: true
 	},
 	rules: {
+		'a11y-click-events-have-key-events': 'off',
+		'svelte-ignore/a11y-click-events-have-key-events': 'on',
+		'click-events-have-key-events': 'off',
 		'jsx-a11y/click-events-have-key-events': 'off',
-		'a11y/click-events-have-key-events': 'off'
+		'a11y/click-events-have-key-events': 'off',
+		'vuejs-accessibility/click-events-have-key-events': 'off',
+		'sveltejs-accessibility/click-events-have-key-events': 'off',
+		'svelte-accessibility/click-events-have-key-events': 'off'
 	}
 };
