b8b9b139960b6fffe1e043ca63567c07bbf043ba
Mark Burggraf <markb@mantisbible.com>
2022-11-23 10:31:14 -0800

    a11y/click-events-have-key-events rule turned off

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,7 @@ module.exports = {
 	ignorePatterns: ['*.cjs'],
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	settings: {
-		'svelte3/typescript': () => require('typescript')
+		'svelte3/typescript': () => require('typescript'),
 	}, 
 	parserOptions: {
 		sourceType: 'module',
@@ -16,5 +16,9 @@ module.exports = {
 		browser: true,
 		es2017: true,
 		node: true
+	},
+	rules: {
+		'jsx-a11y/click-events-have-key-events': 'off',
+		'a11y/click-events-have-key-events': 'off'
 	}
 };
