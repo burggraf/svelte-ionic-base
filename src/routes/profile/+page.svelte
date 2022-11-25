5dcf36dd8fad48d0bac059164a40847cdf039a1b
Mark Burggraf <markb@mantisbible.com>
2022-11-25 11:03:27 -0800

    setup env

---
+++ src/routes/profile/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	// goto('/components/Splash');
+</script>
+<ion-header>
+	<ion-toolbar translucent="true">
+		<ion-buttons slot="start">
+			<ion-menu-button />
+		</ion-buttons>
+		<ion-title>profile page</ion-title>
+	</ion-toolbar>
+</ion-header>
+<ion-content class="ion-padding">
+	profile page
+</ion-content>
