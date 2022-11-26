3b474b24c94599a336808c85460d31cdacb7806e
Mark Burggraf <markb@mantisbible.com>
2022-11-26 07:10:21 -0800

    deploy test

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
 npm run build && mkdir -p ./docs && cp -r ./build/* ./docs && touch ./docs/.nojekyll && rm -r build
-git add docs/.nojekyll
 git add docs
-
+git add docs/.nojekyll
--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static'
 import preprocess from 'svelte-preprocess';
 
+const dev = "production" === "development";
+
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
@@ -8,11 +10,17 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
+			pages: 'docs', // was build, but we want to deploy to GitHub Pages
+			assets: 'docs', // was build, but we want to deploy to GitHub Pages
 			fallback: 'index.html',
 			precompress: false
 		}),
+        paths: {
+            // change below to your repo name
+            base: dev ? "" : "/svelte-ionic-base",
+        },
+		// hydrate the <div id="svelte"> element in src/app.html	
+		// target: "#svelte",	
 		alias: {
 			'$ionic': 'src/lib/ionic',
 			'$ionpage': 'src/lib/ionic/svelte/components/IonPage.svelte',
